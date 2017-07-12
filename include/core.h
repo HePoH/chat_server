e8a54ffc18f5f521b456f57aff4ec93171c4c6ce
Dmitry Medvedev <demon0900@mail.ru>
2017-07-12 16:54:10 +0600

    second commit

--- include/core.h
+++ include/core.h
@@ -8,6 +8,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
+#include <pthread.h>
 #include <fcntl.h>
 #include <mqueue.h>
 
@@ -15,27 +16,50 @@
 #define CLIENT_QUEUE_NAME "/queue-chat-client"
 
 #define QUEUE_PERMISSIONS 0666
-#define MAX_MESSAGES 256
-#define MAX_MSG_SIZE 256
-#define MSG_BUFFER_SIZE MAX_MSG_SIZE + 10
+#define MAX_MESSAGES  256L
+#define MAX_MSG_SIZE 256L
+#define MAX_NAME_SIZE 32
 
-enum CHAT_EVENTS {
+#define SERVER_MSG_SIZE sizeof(SERVER_MSG)
+#define CLIENT_MSG_SIZE sizeof(CLIENT_MSG)
+
+enum CLIENT_EVENTS {
 	CONNECTED,
 	CHANGE_NAME,
 	DISCONNECTED
-}
+};
 
-typedef struct msg {
-	char name[MAX_MSG_SIZE];
-	pid_t pid;
-} MSG;
+enum CLIENT_MSG_TYPE {
+	PUBLIC,
+	PRIVATE,
+	ADM_MSG
+};
+
+typedef struct srv_msg {
+	int msg_type;
+
+	char data[MAX_MSG_SIZE];
+} SERVER_MSG;
+
+typedef struct cl_msg {
+        int msg_type;
+
+        char snd_name[MAX_NAME_SIZE];
+        char rcp_name[MAX_NAME_SIZE];
+
+        pid_t snd_pid;
+        pid_t rcp_pid;
+} CLIENT_MSG;
 
 typedef struct cil {
-	char name[MAX_MSG_SIZE];
-	pid_t pid;
+	char cln_name[MAX_NAME_SIZE];
+	pid_t cln_pid;
 
 	struct cil *next;
 	struct cil *prev;
 } CLIENTS_INFO_LIST;
+
+void* srv_event_hndl(void* args);
+void* cln_msg_hndl(void* args);
 
 #endif
--- lib/core.c
+++ lib/core.c
@@ -1,1 +1,107 @@
 #include "../include/core.h"
+
+void* srv_event_hndl(void* args) {
+	mqd_t qd_srv;
+	struct mq_attr srv_attr;
+	ssize_t bts_num;
+	SERVER_MSG srv_msg;
+
+	srv_attr.mq_flags = 0L;
+	srv_attr.mq_maxmsg = MAX_MESSAGES;
+	/*srv_attr.mq_msgsize = MAX_MSG_SIZE;*/
+	srv_attr.mq_msgsize = 1024L;
+	srv_attr.mq_curmsgs = 0L;
+
+	qd_srv = mq_open(SERVER_QUEUE_NAME, O_RDWR | O_CREAT, QUEUE_PERMISSIONS, &srv_attr);
+	if (qd_srv == -1) {
+		perror ("Server: mq_open(server)");
+		pthread_exit(EXIT_FAILURE);
+	}
+
+	while (1) {
+		bts_num = mq_receive(qd_srv, (char*)&srv_msg, SERVER_MSG_SIZE, NULL);
+		if (bts_num == -1) {
+			perror("Server: mq_receive(server)");
+			pthread_exit(EXIT_FAILURE);
+		}
+
+		printf("Server: mq_receive(server) message received\n");
+		printf("Struct: SERVER_MSG { msg_type = %d, data = %s }\n", srv_msg.msg_type, srv_msg.data);
+
+		/*bts_num = mq_send(qd_srv, (char*)&srv_msg, SERVER_MSG_SIZE, 0);
+		if (bts_num == -1) {
+			perror("Server: mq_send(server)");
+			continue;
+		}
+
+		printf("Server: mq_send(server) response sent to client\n");
+		printf("Struct: SERVER_MSG { msg_type = %d, data = %s }\n", srv_msg.msg_type, srv_msg.data);*/
+	}
+
+	if (mq_close(qd_srv) == -1) {
+		perror("Server: mq_close(server)");
+		pthread_exit(EXIT_FAILURE);
+	}
+
+	if (mq_unlink(SERVER_QUEUE_NAME) == -1) {
+		perror("Server: mq_unlink(server)");
+		pthread_exit(EXIT_FAILURE);
+        }
+
+	printf("Server: server thread bye\n");
+	pthread_exit(EXIT_SUCCESS);
+}
+
+void* cln_msg_hndl(void* args) {
+	mqd_t qd_cln;
+	struct mq_attr cln_attr;
+	ssize_t bts_num;
+	CLIENT_MSG cln_msg;
+
+	cln_attr.mq_flags = 0L;
+	cln_attr.mq_maxmsg = MAX_MESSAGES;
+	/*cln_attr.mq_msgsize = MAX_MSG_SIZE;*/
+	cln_attr.mq_msgsize = sizeof(CLIENT_MSG);
+	cln_attr.mq_curmsgs = 0L;
+
+	qd_cln = mq_open(CLIENT_QUEUE_NAME, O_RDWR | O_CREAT, QUEUE_PERMISSIONS, &cln_attr);
+	if (qd_cln == -1) {
+		perror ("Server: mq_open(client)");
+		pthread_exit(EXIT_FAILURE);
+	}
+
+	while (1) {
+		bts_num = mq_receive(qd_cln, (char*)&cln_msg, 1024, NULL);
+		if (bts_num == -1) {
+			perror("Server: mq_receive(client)");
+			pthread_exit(EXIT_FAILURE);
+		}
+
+		printf("Server: mq_receive(client) message received\n");
+		printf("Struct: CLIENT_MSG { msg_type = %d, sender = %s [PID: %d], recipient = %s [PID: %d] }\n", cln_msg.msg_type, cln_msg.snd_name,
+		cln_msg.snd_pid, cln_msg.rcp_name, cln_msg.rcp_pid);
+
+		/*bts_num = mq_send(qd_cln, (char*)&cln_msg, CLIENT_MSG_SIZE, 0);
+		if (bts_num == -1) {
+			perror("Server: mq_send(client)");
+			continue;
+		}
+
+		printf("Server: mq_send(client) response sent to client\n");
+		printf("Struct: CLIENT_MSG { msg_type = %d, sender = %s [PID: %d], recipient = %s [PID: %d] }\n", cln_msg.msg_type, cln_msg.snd_name,
+		cln_msg.snd_pid, cln_msg.rcp_name, cln_msg.rcp_pid);*/
+	}
+
+	if (mq_close(qd_cln) == -1) {
+		perror("Server: mq_close(client)");
+		pthread_exit(EXIT_FAILURE);
+        }
+
+	if (mq_unlink(CLIENT_QUEUE_NAME) == -1) {
+		perror("Server: mq_unlink(client)");
+		exit(EXIT_FAILURE);
+	}
+
+	printf("Server: client thread bye\n");
+	pthread_exit(EXIT_SUCCESS);
+}
